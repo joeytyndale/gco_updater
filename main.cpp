59f5f326204b01791c913869718701d3471ce38b
Joseph Horning <jhroning@indataconsulting.com>
2020-08-26 10:23:45 -0700

    Added HTTP Header and built quick program that pulls data from api

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
-#include <stdio.h>
-#include <iostream>
-using namespace std;
+#include "HTTPRequest.hpp"
+#include <iostream> 
 
 int main(){
+    try
+    {
+        // you can pass http::InternetProtocol::V6 to Request to make an IPv6 request
+        http::Request request("http://api.joeytyndale.com");
 
-    cout << "Hello World";
+        // send a get request
+        const http::Response response = request.send("GET");
+        std::cout << std::string(response.body.begin(), response.body.end()) << '\n'; // print the result
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Request failed, error: " << e.what() << '\n';
+    }
 
     return 0;
 }
